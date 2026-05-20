b577d7c695dfc30730d190e03804d2bd88224a79
ceratiwong <michaelwong@uamv.edu.ni>
2026-05-20 10:36:19 -0600

    feat: agregue el código de mostrar

--- Program.cs
+++ Program.cs
@@ -13,6 +13,14 @@ int[] notas = new int [25];
 /*agregar*/
 
 /*mostrar*/
+Console.WriteLine("Mostrando notas.");
+for (int i = 0; i < 25; i++)
+{
+    if (notas[i] < 70) Console.ForegroundColor = ConsoleColor.Red;
+    else Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine($"Nota {i + 1}: {notas[i]}");
+    Console.ResetColor();
+}
 
 /*3 primeros lugares*/
 
