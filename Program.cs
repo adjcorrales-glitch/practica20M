7dc1752ef359764b26c598abe618d815b2aaba79
angel <adjcorrales@uamv.edu.ni>
2026-05-20 09:37:26 -0600

    feat: configuraciondeapp

--- Program.cs
+++ Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
+﻿/*crear una aplicacion que guarde lo registros de notas de 25 estudiantes,
+
+debe presentar un menu:
+1. agrega 
+2. mostrar 
+3- Mostrar los 3 primeros lugares
+4.mostar las notas en orden descendente
+5-salir
+*/
